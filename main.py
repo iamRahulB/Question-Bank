e0b171c2bac1b34130b6729cc48550976d2afb17
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-01-22 02:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from quiz_brain import Brain
 length=len(question_data)
 
 
-
+score=0
 correct=True
 
 print("Coded By iamRahul")
@@ -11,7 +11,9 @@ for i in range(len(question_data)):
 	while correct:
 		question=question_data[i]["text"]
 		answer=question_data[i]["answer"]
-		brain=Brain(question,answer,i)
-		correct=brain.check_answer()
+		brain=Brain(question,answer,i,score)
+		correct,nscore=brain.check_answer()
+		score=nscore
+		print(f"score is {nscore}")
 		break
 		
