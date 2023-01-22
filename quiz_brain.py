58194fd383fa3c876e6e87dcc9d8ee8a9f2e920f
Rahul Bhole <108116259+iamRahulB@users.noreply.github.com>
2023-01-22 15:35:51 +0530

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,17 +7,19 @@ class Brain:
 	
 	def check_answer(self):
 		self.i+=1
-		user_answer=input(f"Q.{self.i} {self.question} (True\False) :\n")
+		user_answer=input(f"Q.{self.i} {self.question} (True\False) :\n").capitalize()
 		score_user=self.scorew()
-		if user_answer==self.answer:
-			
-			return self.answer,score_user;
-			
-		else:
-			print("sorry u are not Rahul to answer everything .. hahahaha")
-			return False
+		
+		self.answer,score_user=self.check_ans(user_answer,score_user)
+		return self.answer,score_user;
 			
 	def scorew(self):
 		self.score+=1
 		return self.score
-		
+	def check_ans(self,user_answer,score_user):
+		if user_answer==self.answer:
+			print("correct")
+		elif user_answer!=self.answer:
+			self.answer=False
+			score_user-=1
+		return self.answer,score_user;	
